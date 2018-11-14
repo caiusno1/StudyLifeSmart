551fef3b61bac3edc3ebc5eca2e9e6bca28c57e2
Kai Biermeier <kbiermeier1@web.de>
2018-11-14 19:32:50 +0100

    implement global socketchannel (to implement multiple channels finally in future work)

--- test-server/server.js
+++ test-server/server.js
@@ -1,15 +1,20 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-
+let socketlist=[]
 io.on('connection', (socket) => {
 
     // Log whenever a user connects
     console.log('user connected');
+    socketlist.push(socket)
 
     // Log whenever a client disconnects from our websocket server
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        var index = socketlist.indexOf(5);
+        if (index > -1) {
+            socketlist.splice(index, 1);
+        }
     });
 
     // When we receive a 'message' event from our client, print out
@@ -17,7 +22,9 @@ io.on('connection', (socket) => {
     // using `io.emit()`
     socket.on('message', (message) => {
         console.log("Message Received: " + message);
-        io.emit('message', {type:'new-message', text: message});    
+        socketlist.forEach(function(cur){
+            cur.emit('message', {type:'new-message', text: message});
+        });
     });
 });
 

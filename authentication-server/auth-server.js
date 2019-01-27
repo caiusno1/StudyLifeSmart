1a2524f49b4e4f6cf510b3574ffc12bd67c74eeb
Philipp <Philipp.Brinkmann8497@web.de>
2019-01-27 18:22:45 +0100

    added sqlite database for user-data

--- authentication-server/auth-server.js
+++ authentication-server/auth-server.js
@@ -3,16 +3,29 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json({extended:true}));
+var path = require('path')
+var dbPath = path.resolve(__dirname, 'database.db')
+const sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbPath, (err) => {if(err) {return console.error(err.message)} console.log('Connected!');});
+//db.run('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)');
+db.close();
 let http = require('http').Server(app);
+
 
 app.post('/users/authenticate', function (req, res) {
   console.log('got a post: '+req.body.password+', '+req.body.username);
   //todo: database connection
-  if(req.body.username=='admin' && req.body.password=='password') {
-    res.send(JSON.stringify('authenticated'));
-  } else {
-    res.send(JSON.stringify('failed'));
-  }
+  db = new sqlite3.Database(dbPath, (err) => {if(err) {return console.error(err.message)}});
+  var statement = db.prepare('SELECT id FROM users WHERE username = ? AND password = ?', [req.body.username, req.body.password]);
+  statement.get(function(err, result) {
+      if(err) {console.error(err.message);
+    }
+    if(result) {
+      console.log(result);
+      res.send(JSON.stringify('authenticated'));
+    } else {
+      res.send(JSON.stringify('failed'));
+  }});
 });
 
 app.get('/', function(req, res) {

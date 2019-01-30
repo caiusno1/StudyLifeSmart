95bfc18e7df04b55da1d6f9fd407034d06996980
Philipp <Philipp.Brinkmann8497@web.de>
2019-01-30 16:32:04 +0100

    passwords are now stored as hashes

--- authentication-server/auth-server.js
+++ authentication-server/auth-server.js
@@ -3,29 +3,37 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json({extended:true}));
-var path = require('path')
-var dbPath = path.resolve(__dirname, 'database.db')
+var path = require('path');
+var dbPath = path.resolve(__dirname, 'database.db');
 const sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbPath, (err) => {if(err) {return console.error(err.message)} console.log('Connected!');});
-//db.run('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)');
+db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)');
 db.close();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 let http = require('http').Server(app);
 
 
 app.post('/users/authenticate', function (req, res) {
   console.log('got a post: '+req.body.password+', '+req.body.username);
   db = new sqlite3.Database(dbPath, (err) => {if(err) {return console.error(err.message)}});
-  var statement = db.prepare('SELECT id FROM users WHERE username = ? AND password = ?', [req.body.username, req.body.password]);
+  var statement = db.prepare('SELECT id, password FROM users WHERE username = ?', [req.body.username]);
   statement.get(function(err, result) {
     if(err) {console.error(err.message);}
     if(result) {
       console.log(result);
-      res.send(JSON.stringify('authenticated'));
+      bcrypt.compare(req.body.password, result.password, function(err, res2) {
+        if(err) {console.error(err.message);}
+        if(res2) {
+          res.send(JSON.stringify('authenticated'));
+        } else {
+          res.send(JSON.stringify('failed'));
+        }
+      });
     } else {
       res.send(JSON.stringify('failed'));
   }});
   //todo: track currently active users (needs http request on client logout/timeout)
-  //todo: hash passwords
 });
 
 app.post('/users/register', function(req, res) {
@@ -36,12 +44,17 @@ app.post('/users/register', function(req, res) {
     if(err) {console.error(err.message);
     }
     if(!result) {
-      console.log(result);
-      var statement2 = db.prepare('INSERT INTO users(username, password) VALUES (?, ?);', [req.body.username, req.body.password]);
-      statement2.get(function(err, result) {
+      bcrypt.genSalt(saltRounds, function(err, salt) {
         if(err) {console.error(err.message);}
-        if(result) {console.log(result);}
-        res.send(JSON.stringify('registered'));
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          if(err) {console.error(err.message);}
+            var statement2 = db.prepare('INSERT INTO users(username, password) VALUES (?, ?);', [req.body.username, hash]);
+            statement2.get(function(err, result) {
+              if(err) {console.error(err.message);}
+              console.log('New user data has been added to the DB.');
+              res.send(JSON.stringify('registered'));
+          });
+        });
       });
     } else {
       res.send(JSON.stringify('failed'));
